ceccec17b2af536e6be23393ec43a01bc5e3de88
petertorelli <peter.j.torelli@gmail.com>
2022-08-17 16:03:47 -0700

    Added MIT license to everything.

--- mbed3_launch_client.bash
+++ mbed3_launch_client.bash
@@ -1,3 +1,22 @@
+# Copyright (c) 2022 EEMBC
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 #!/usr/bin/env bash
 
 MBED_CLIENT_DIR=~/github/Mbed-TLS/mbedtls/build/programs/ssl
@@ -5,13 +24,19 @@ CERT_DIR=/home/ptorelli/github/eembc/sec-trace/mycerts
 COMMAND_DIR=/home/ptorelli/github/eembc/sec-trace
 
 mode=$1
-LOG=$PWD/log.mbed3.$mode
-TABLE=$PWD/table.mbed3.$mode
+prefix=$2
+if [[ "$prefix" != "" ]] ; then
+    prefix="$prefix."
+fi
+LOG=$PWD/${prefix}log.mbed3.$mode
+TABLE=$PWD/${prefix}table.mbed3.$mode
+XCHECK=$PWD/${prefix}xcheck.mbed3.$mode
 
 case $mode in
 high|medium|light)
 	gdb -command=$COMMAND_DIR/mbed3_command_$mode.gdb $MBED_CLIENT_DIR/ssl_client2 > $LOG
     $COMMAND_DIR/process_gdb_trace_mbed3.py $LOG > $TABLE
+    $COMMAND_DIR/sum_crosscheck.pl $TABLE > $XCHECK
     ;;
 *)
 	echo "Mode '$mode' is not valid; must be light, medium, high"
